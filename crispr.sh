77416322abc06d9d2158b620de9a268e7d35d6ac
Jasmin Zohren <5310751+jzohren@users.noreply.github.com>
2020-08-06 18:45:27 +0200

    Update crispr.sh
    included infoFile variable

--- crispr.sh
+++ crispr.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # example execution:
-# ./crispr.sh A1 true
+# ./crispr.sh A15 true info_file_example
 
 sample=$1
 map=$2
+infoFile=$3
 
 genome=/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa
 
@@ -13,10 +14,11 @@ outDir=/user/home/working_dir/results
 
 if [ $map = true ]
 then
-	echo "running read mapping"
+	echo "running read mapping with bwa and samtools sort-index"
 
 	bwa mem -t 4 $genome $fastqDir/${sample}_R1.fastq.gz $fastqDir/${sample}_R2.fastq.gz | samtools view -b - | samtools sort - -o $outDir/${sample}.bam
 	samtools index $outDir/${sample}.bam
 fi
 
-Rscript crisprvar.r $sample
+echo "running CrispRVariants analysis including creation of plots"
+Rscript crisprvar.r $sample $infoFile
