cee7de3c83737a7e2a4649cf5bd7d29a632fae44
Jasmin Zohren <5310751+jzohren@users.noreply.github.com>
2020-08-06 14:19:47 +0200

    Update crispr.sh
    simplified script to be platform independent

--- crispr.sh
+++ crispr.sh
@@ -1,68 +1,22 @@
 #!/bin/bash
-# crispr analysis of MiSeq data
-#SBATCH --job-name=crispr
-#SBATCH --ntasks=4
-#SBATCH --time=01:00:00
-#SBATCH --mem=10G
-#SBATCH --partition=cpu
-#SBATCH --array=1-10
-#SBATCH --output=crispr_%A_%a.out
 
-# action required:
-# change array parameter (above) to the number of files to analyse (i.e. number of lines in readFiles)
+# example execution:
+# ./crispr.sh A1 true
 
-echo 'JOB STARTED'
-date
+sample=$1
+map=$2
 
+genome=/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa
 
-# action required:
-# select your genome file here and modify path to go through your lab
-GENOME=/camp/svc/reference/Genomics/aws-igenomes/Mus_musculus/UCSC/mm10/Sequence/BWAIndex/genome.fa
-
-SEQRUN=200630
-MAP=true
+fastqDir=/user/home/working_dir/fastq
+outDir=/user/home/working_dir/results
 
 if [ $MAP = true ]
 then
-
 	echo "running read mapping"
-	
-	# action required:
-	# change name and potentially path to readFiles
-	READ1=$(sed -n "${SLURM_ARRAY_TASK_ID}p" readFiles_$SEQRUN)
-	READ2="${READ1/_R1_/_R2_}"
-	LIB=$(echo $READ1 | rev | cut -d "/" -f 1 | rev | cut -d "_" -f 1)
 
-
-	# action required:
-	# specify path where to store bam files
-	OUT=/camp/lab/turnerj/working/Charlotte/bams/$SEQRUN/
-	mkdir -p $OUT
-	cd $OUT
-	echo "cd $OUT"
-
-	ml BWA
-	bwa mem -t 4 $GENOME $READ1 $READ2 > "$LIB".sam
-
-	ml purge    
-	ml SAMtools
-
-	samtools view -b "$LIB".sam -o "$LIB".bam
-	samtools sort "$LIB".bam -o "$LIB".sorted.bam
-	samtools index "$LIB".sorted.bam
-	rm "$LIB".sam "$LIB".bam 
-
+	bwa mem -t 4 $genome $fastqDir/${sample}_R1.fastq.gz $fastqDir/${sample}_R2.fastq.gz | samtools view -b - | samtools sort - -o $outDir/${sample}.bam
+	samtools index $outDir/${sample}.bam
 fi
 
-
-ml Anaconda2 SAMtools
-source activate r_3.6
-
-DIR=/camp/lab/turnerj/working/Charlotte/scripts
-cd $DIR
-echo "cd $DIR"
-
-Rscript crisprvar.r $SLURM_ARRAY_TASK_ID $SEQRUN
-
-date
-echo 'JOB ENDED'
+Rscript crisprvar.r $sample
