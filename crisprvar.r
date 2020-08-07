ab1f17d91bbc215c048d1050a9bfdbd086369871
Jasmin Zohren <5310751+jzohren@users.noreply.github.com>
2020-08-07 17:25:30 +0200

    Update crisprvar.r
    fixed formatting

--- crisprvar.r
+++ crisprvar.r
@@ -9,17 +9,17 @@ library(glue)
 
 # variable definitions (provided to/from crispr.sh script)
 
-args <- commandArgs(trailingOnly = TRUE)
-sample <- args[1]
-info_file <- read.table(args[2], sep = "\t")
-genome <- args[3]
-out_dir <- args[4]
+args 		<- commandArgs(trailingOnly = TRUE)
+sample 		<- args[1]
+info_file 	<- read.table(args[2], sep = "\t")
+genome 		<- args[3]
+out_dir 	<- args[4]
 
 setwd(out_dir)
 colnames(info_file) <- c("sample", "gene", "seq", "chr", "start", "end", "strand", "plus", "minus")
-gene <- as.character(info_file$gene)
-sample_name <- glue("{sample}_{gene}")
-bam <- glue("{out_dir}/{sample}.bam")
+gene 				<- as.character(info_file$gene)
+sample_name 		<- glue("{sample}_{gene}")
+bam 				<- glue("{out_dir}/{sample}.bam")
 
 # create GRanges object of area around guide sequence
 
@@ -29,8 +29,8 @@ region <- GRanges(seqnames = info_file$chr, ranges = IRanges(info_file$start - i
 # the 'region' object is used to extract a short FASTA sequence from the genome file
 
 system(sprintf(glue("samtools faidx {genome} %s:%s-%s > %s.seq"), seqnames(region), start(region), end(region), sample_name))
-fasta <- read.fasta(glue("{sample_name}.seq"), as.string = T)
-reference <- DNAString(unlist(fasta))
+fasta 		<- read.fasta(glue("{sample_name}.seq"), as.string = T)
+reference 	<- DNAString(unlist(fasta))
 system(sprintf("rm %s.seq", sample_name))
 
 if (info_file$strand == "-") {
@@ -41,8 +41,8 @@ if (info_file$strand == "-") {
 
 crispr_set <- readsToTarget(bam, region, reference = reference, target.loc = 17 + info_file$minus, collapse.pairs = T, chimera.to.target = 200, names = sample_name)
 
-var_counts <- variantCounts(crispr_set)
-eff_raw <- mutationEfficiency(crispr_set)
+var_counts 	<- variantCounts(crispr_set)
+eff_raw 	<- mutationEfficiency(crispr_set)
 
 # information about mutation efficancy and allele frequencies are written to simple text files
 
@@ -67,9 +67,10 @@ write.table(var_counts, file = glue("{sample_name}_variants.txt"), sep = "\t", q
 # the variants occurring most frequently are plotted in a pie chart
 # both with and without the "no variant" category
 
-selection <- which(var_counts > 99)
-top_mutations <- var_counts[selection]
+selection 		<- which(var_counts > 99)
+top_mutations 	<- var_counts[selection]
 mut_percentages <- round(100 * (var_counts / sum(var_counts)), 2)
+
 pie_labels <- rownames(var_counts)[selection]
 pie_labels <- glue("{mut_percentages[selection]} % {pie_labels}")
 pie_colors <- sample(colors(), length(top_mutations))
