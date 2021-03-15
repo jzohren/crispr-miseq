5c10b64bc0de46e4936746de4850acc7ed760750
Jasmin Zohren <5310751+jzohren@users.noreply.github.com>
2021-03-15 11:51:31 +0100

    tidy up indentation

--- crisprvar.r
+++ crisprvar.r
@@ -10,15 +10,15 @@ library(glue)
 # variable definitions (provided to/from crispr.sh script)
 
 args      <- commandArgs(trailingOnly = TRUE)
-sample 		<- args[1]
+sample    <- args[1]
 info_file <- read.csv(args[2])
-genome 		<- args[3]
-out_dir 	<- args[4]
+genome    <- args[3]
+out_dir   <- args[4]
 
 setwd(out_dir)
-gene 				<- as.character(info_file$gene)
+gene        <- as.character(info_file$gene)
 sample_name <- glue("{sample}_{gene}")
-bam 				<- glue("{out_dir}/{sample}.bam")
+bam         <- glue("{out_dir}/{sample}.bam")
 
 # create GRanges object of area around guide sequence
 
@@ -28,7 +28,7 @@ region <- GRanges(seqnames = info_file$chr, ranges = IRanges(info_file$start - i
 # the 'region' object is used to extract a short FASTA sequence from the genome file
 
 system(sprintf(glue("samtools faidx {genome} %s:%s-%s > %s.seq"), seqnames(region), start(region), end(region), sample_name))
-fasta 		<- read.fasta(glue("{sample_name}.seq"), as.string = T)
+fasta     <- read.fasta(glue("{sample_name}.seq"), as.string = T)
 reference <- DNAString(unlist(fasta))
 system(sprintf("rm %s.seq", sample_name))
 
@@ -40,8 +40,8 @@ if (info_file$strand == "-") {
 
 crispr_set <- readsToTarget(bam, region, reference = reference, target.loc = 17 + info_file$minus, collapse.pairs = T, chimera.to.target = 200, names = sample_name)
 
-var_counts 	<- variantCounts(crispr_set)
-eff_raw 	<- mutationEfficiency(crispr_set)
+var_counts <- variantCounts(crispr_set)
+eff_raw    <- mutationEfficiency(crispr_set)
 
 # information about mutation efficancy and allele frequencies are written to simple text files
 
@@ -66,8 +66,8 @@ write.table(var_counts, file = glue("{sample_name}_variants.txt"), sep = "\t", q
 # the variants occurring most frequently are plotted in a pie chart
 # both with and without the "no variant" category
 
-selection 		<- which(var_counts > 99)
-top_mutations 	<- var_counts[selection]
+selection       <- which(var_counts > 99)
+top_mutations   <- var_counts[selection]
 mut_percentages <- round(100 * (var_counts / sum(var_counts)), 2)
 
 pie_labels <- rownames(var_counts)[selection]
@@ -80,4 +80,3 @@ pdf(file = glue("{sample_name}_pie.pdf"), width = 9.5, height = 7)
  pie(top_mutations[-1], labels = NA, col = pie_colors, radius = 0.9)
  legend("right", pie_labels[-1], col = pie_colors, pch = 15)
 dev.off()
-
