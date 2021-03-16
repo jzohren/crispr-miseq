1cb53fc28f6c0849067b21a89e4f0e5aca979358
Jasmin Zohren <5310751+jzohren@users.noreply.github.com>
2021-03-16 12:32:24 +0100

    Change all parameters to command line

--- crispr.sh
+++ crispr.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-# example execution: ./crispr.sh A15 info_file.csv true
-# where 'A15' is the sample name
-# and 'info_file.csv' contains information about the guide sequence
-# and 'true/false' indicates whether the mapping step should be executed
+# example execution: ./crispr.sh A15 info_file.csv true genome.fa fastqDir outDir
+# where:
+# 'A15' is the sample name
+# 'info_file.csv' contains information about the guide sequence
+# 'true/false' indicates whether the mapping step should be executed
+# 'genome.fa' is the full path to the genome file (bwa and fai index files need to be in the same directory)
+# 'fastqDir' is the directory with the fastq files
+# 'outDir' is the directory for the results
+
 
 # variable definitions from command line parameters
 
 sample=$1
 infoFile=$2
 map=$3
+genome=$4
+fastqDir=$5
+outDir=$6
 
-# user-defined variable definitions
-
-# provide full path to genome file
-# bwa and fai index files need to be in the same directory
-genome=<path to genome.fa>
-# provide full path to directory with fastq files
-fastqDir=<path to fastq files>
-# provide an output directory for the results
-outDir=<path to output directory>
-
-# only run mapping stage if variable set to true
+# only run mapping stage if 'map' variable is set to true
 # explanation: usually, the read mapping only has to be done once
 # however, you might want to re-run the analysis in R several times
 
@@ -35,7 +33,7 @@ then
 fi
 
 # continue analysis in R
-# provide relevant parameters to R script 
+# relevant parameters are provided to the R script 
 
 echo "running CrispRVariants analysis including creation of plots"
 Rscript crisprvar.r $sample $infoFile $genome $outDir
